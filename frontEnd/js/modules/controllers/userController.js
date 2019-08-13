2e843d470b794efadb18949037ee0124fede306e
octavia30 <37707065+octavia30@users.noreply.github.com>
2019-08-13 14:40:07 +0300

    register final

--- frontEnd/js/modules/controllers/userController.js
+++ frontEnd/js/modules/controllers/userController.js
@@ -2,17 +2,22 @@ app.controller("userController",['$scope', 'userService','$location',
 function($scope, userService, $location){
   //app.controller("userController",function($scope, $location){
   $scope.box=false;
+  var path;
+  if($scope.box == false){
+    path = '/client';
+  }else{
+    path = '/contractor';}
   
 	$scope.submit = function() {
 
     console.log("in controller");
-    var userPath = userService.path; 
+    //var userPath = userService.path; 
 
     userService.registerUser($scope.user, $scope.box)
-    .then(function(userPath){
+    .then(function(){
       console.log("SUCCES");
-      //console.log($scope.user)
-      $location.path(userPath);
+      console.log(path);
+      $location.path(path);
     },function(){
     console.log("ERROR in controller")
   })
--- frontEnd/js/modules/services/userService.js
+++ frontEnd/js/modules/services/userService.js
@@ -1,46 +1,37 @@
 app.factory("userService" , function($http) {
-
+    
     function registerUser(user, box) {
         if(box == false){
             var url = 'http://localhost:8080/Users';
-            var path = '/client';
+            //path = '/client';
             var data = {
-                first_name: user.nume,
-                last_name: user.lastname,
+                name: user.name,
+                lastName: user.lastName,
                 email: user.email,
-                telefon: user.tel
+                mobile: user.mobile,
+                password: user.password
             }
         }
         else{
             var url = 'http://localhost:8080/Users/Contractor';
-            var path = '/contractor';
+            //path = '/contractor';
             var data = {
-                first_name: user.name,
-                last_name: user.lastname,
+                name: user.name,
+                lastName: user.lastName,
                 email: user.email,
-                telefon: user.tel,
+                mobile: user.mobile,
                 stage_name: user.numeStage,
                 canalYt: user.yt,
                 tip: user.tip,
                 stil: user.stil
             }
         }
-        /*
-        return $http.post(url, data).then(
-            function RegSucces(response){
-                console.log("succes");
-                console.log(data);
-                return data;
-            }, function RegErr(){
-                console.log("error in service");
-            }
-        )
-    }*/
 
         return $http({
             method: 'POST',
             url: url,
             data: data,
+            path :path
             
         }).then(function regSucces(response) {
             console.log("succes post")
